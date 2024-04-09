fd5b634f4366051e4187cb368076a285fc6a943c
Mutai05 <kelvok2014@gmail.com>
2024-04-09 16:45:02 -0700

    Fixed pycodestyle format

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,23 +7,26 @@ posts for a given subreddit.
 import requests
 import sys
 
+
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Custom User Agent"}
+    """Query Reddit and print titles of the first 10 hot posts"""
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
+    # Set a custom User-Agent to avoid too many requests error
+    headers = {'User-Agent': 'My user Agent 1.0'}
+
+    # send a GET request to the Reddit API
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        for post in posts:
-            print(post['data']['title'])
-    else:
-        print(None)
-
-if __name__ == "__main__":
-    subreddit = sys.argv[1] if len(sys.argv) > 1 else None
-    if subreddit:
-        top_ten(subreddit)
-    else:
-        print("Please pass an argument for the subreddit to search.")
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
+        print("None")
